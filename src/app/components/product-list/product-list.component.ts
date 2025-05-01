c2740b11fddbc465c57fa630274f23932c8f3480
hendrowunga <hendrowunga073@gmail.com>
2025-05-02 03:12:07 +0700

    update ; product list component.ts

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -54,9 +54,12 @@ export class ProductListComponent implements OnInit {
     if (hasCategoryId) {
       // get the "id" param string. convert string to a number using the "+" symbol
       this.currentCategoryId = +this.route.snapshot.paramMap.get('id')!;
+      // get the "name" param string
+      this.currentCategoryName = this.route.snapshot.paramMap.get('name')!;
     } else {
       // not category id available ... default to category id 1
       this.currentCategoryId = 1;
+      this.currentCategoryName = 'Books';
     }
 
     // now get the products for the given category id
