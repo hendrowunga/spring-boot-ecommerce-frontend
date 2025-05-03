6f0c98159220ee279aabaa9b7587cebb7bbbe7c7
hendrowunga <hendrowunga073@gmail.com>
2025-05-03 14:39:19 +0700

    feat(app.component): initialize root component with core UI modules
    Initialize app.component.ts as the main root component and configure essential modules for the application, including RouterModule, NgbModule, and core UI components (ProductCategoryMenu, Search, CartStatus).
    
    Inisialisasi app.component.ts sebagai komponen root utama dan konfigurasikan modul inti aplikasi, termasuk RouterModule, NgbModule, dan komponen UI inti (ProductCategoryMenu, Search, CartStatus).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,23 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { ProductCategoryMenuComponent } from './components/product-category-menu/product-category-menu.component';
 import { SearchComponent } from './components/search/search.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CartStatusComponent } from './components/cart-status/cart-status.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  // RouterOutlet,ProductListComponent,
   imports: [
     // RouterOutlet,
     // CommonModule,
+    // ProductListComponent,
+    NgbModule,
     RouterModule,
     ProductCategoryMenuComponent,
     SearchComponent,
+    CartStatusComponent,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
