3877ece59d708492125b5c807bf3bde52bf2b3f8
hendrowunga <hendrowunga073@gmail.com>
2025-05-03 12:15:49 +0700

    feat(cart-status): add cart status component to display total price and quantity
    Added a standalone CartStatusComponent that shows the current cart total price and quantity.
    Includes template and component logic to subscribe to CartService observables (totalPrice and totalQuantity).
    Provides a link to navigate to the cart details page.

---
+++ src/app/components/cart-status/cart-status.component.ts
@@ -0,0 +1,32 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+
+@Component({
+  selector: 'app-cart-status',
+  imports: [CommonModule, RouterModule],
+  standalone: true,
+  templateUrl: './cart-status.component.html',
+  styleUrl: './cart-status.component.css',
+})
+export class CartStatusComponent implements OnInit {
+  totalPrice: number = 0.0;
+  totalQuantity: number = 0;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.updateCartStatus();
+  }
+
+  updateCartStatus() {
+    // subscribe to the cart totalPrice
+    this.cartService.totalPrice.subscribe((data) => (this.totalPrice = data));
+
+    // subscribe to the cart totalQuantity
+    this.cartService.totalQuantity.subscribe(
+      (data) => (this.totalQuantity = data)
+    );
+  }
+}
