a063c9067978aa593a21b277d621d2d733514624
hendrowunga <hendrowunga073@gmail.com>
2025-05-03 14:40:47 +0700

    feat(app.routes): setup initial application routes
    Configured main application routes for product listing, product details, cart, checkout, and search functionality. Includes fallback redirects. (Konfigurasi rute utama aplikasi untuk daftar produk, detail produk, keranjang, pembayaran, dan pencarian. Termasuk pengalihan fallback.)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { ProductListComponent } from './components/product-list/product-list.component';
-// Definisikan dan ekspor konstanta 'routes' dengan tipe Routes
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
+import { CartDetailsComponent } from './components/cart-details/cart-details.component';
+import { CheckoutComponent } from './components/checkout/checkout.component';
 export const routes: Routes = [
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'cart-details', component: CartDetailsComponent },
+  { path: 'products/:id', component: ProductDetailsComponent },
   { path: 'search/:keyword', component: ProductListComponent },
-  { path: 'category/:id/:name', component: ProductListComponent },
+  { path: 'category/:id', component: ProductListComponent },
   { path: 'category', component: ProductListComponent },
   { path: 'products', component: ProductListComponent },
   { path: '', redirectTo: '/products', pathMatch: 'full' },
