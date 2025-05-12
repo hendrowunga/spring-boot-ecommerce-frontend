bdf236c72ed070482b1342baa8c3dd2f3e9ad6df
hendrowunga <hendrowunga073@gmail.com>
2025-05-12 20:30:45 +0700

    Add Okta OIDC configuration

---
+++ src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+  oidc: {
+    clientId: '0oaonroaf8uwkc3EP5d7',
+    issuer: 'https://dev-56297007.okta.com/oauth2/default',
+    redirectUri: 'http://localhost:4200/login/callback',
+    scopes: ['openid', 'profile', 'email'],
+  },
+};
